3c0a19b7dee554234214a09440e6321a68de3ab4
jovan0901 <jovanvelickovic0901@gmail.com>
2021-08-07 10:35:30 +0530

    :lipstick: Add require image path constants

---
+++ lib/constant.dart
@@ -0,0 +1,37 @@
+const destination = [
+  {
+    'imagePath': 'assets/tower.jpg'
+  }, 
+  {
+    'imagePath': 'assets/mountain.jpg'
+  }, 
+  {
+    'imagePath': 'assets/city.jpg'
+  }, 
+  {
+    'imagePath': 'assets/statue.jpg'
+  }
+];
+
+const hotDestination = [
+  {
+    'imagePath': 'assets/bridge.jpg',
+    'placeName': 'America',
+    'placeCount': '18 Tourist plcae'
+  },
+  {
+    'imagePath': 'assets/japan.jpg',
+    'placeName': 'Japan',
+    'placeCount': '10 Tourist plcae'
+  },
+  {
+    'imagePath': 'assets/mountain.jpg',
+    'placeName': 'Newyork',
+    'placeCount': '20 Tourist plcae'
+  },
+  {
+    'imagePath': 'assets/city.jpg',
+    'placeName': 'Newyork',
+    'placeCount': '18 Tourist plcae'
+  },
+];
